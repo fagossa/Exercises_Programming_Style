ddf71d4dd8ce477cf0d5839ce849f0197a4cf3c2
Fabian Gutierrez <fagossa@gmail.com>
2016-03-09 22:03:15 +0100

    WIP

--- src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
+++ src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
@@ -4,44 +4,26 @@ object WordFrequencyCounter {
 
   class WordFrequencyController(words: List[String], maxWords: Int) {
 
-    private val specialCharsManager = new SpecialCharsManager(words)
-
-    private val stopWordManager = new StopWordManager()
-
-    private val wordFrequencyManager = new WordFrequencyManager()
-
-    def run: Seq[(String, Int)] = {
-      val withoutSpecialChars = specialCharsManager.removeSpecialChars()
-
-      val withoutStopWords = stopWordManager.removeStopWords(withoutSpecialChars)
-
-      val wordsInGroups = wordFrequencyManager.groupWordsTogether(withoutStopWords)
-      wordFrequencyManager.sortWords(maxWords, wordsInGroups)
-    }
-  }
-
-  class SpecialCharsManager(lines: List[String]) {
-    def removeSpecialChars(): List[String] = {
-      lines.map(_.replaceAll("[,|;|.]", ""))
-    }
-  }
-
-  class StopWordManager() {
     lazy val stopWords = scala.io.Source
       .fromInputStream(getClass.getResourceAsStream("/stop_words.txt")).getLines()
       .mkString.split(",").toList
 
-    private def isNotStopWord(word: String): Boolean = !stopWords.contains(word)
-
-    private def containOnlyLetters(word: String) = word.forall(_.isLetter)
-
-    def removeStopWords(words: List[String]): List[String] = {
+    def run: Seq[(String, Int)] = {
       words
+        .map(_.replaceAll("[,|;|.]", ""))
+        // remove stop words
         .flatMap(_.split(" "))
         .map(_.toLowerCase)
-        .filter(isNotStopWord)
-        .filter(containOnlyLetters)
+        .filter(!stopWords.contains(_))
+        .filter(_.forall(_.isLetter))
         .filterNot(_.isEmpty)
+        //groupWordsTogether
+        .groupBy(identity)
+        .mapValues { t: List[String] => t.size }
+        //sortWords
+        .map { case (k, v) => (k, v) }.toSeq
+        .sortWith { case ((word1, count1), (word2, count2)) => if (count1 == count2) word1.compareTo(word2) <= 0 else count1 > count2 }
+        .take(maxWords)
     }
   }
 
