ec896d8c9f2fbfa10921626a74e2b4d0fe18e881
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2016-04-02 14:51:36 +0200

    first version of customized presentation

--- src/main/public/presentation/index.js
+++ src/main/public/presentation/index.js
@@ -28,25 +28,24 @@ import preloader from "spectacle/lib/utils/preloader";
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
-// Import custom component
-import Interactive from "../assets/interactive";
-
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  author_original: require("../assets/author_original.png"),
+  basic: require("../assets/basic.jpg"),
+  book_original: require("../assets/book-original.jpg"),
+  book_programming: require("../assets/book-programming.jpg"),
+  historical: require("../assets/historical.jpg"),
+  trollface: require("../assets/trollface.png")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#9b2f8b"
 });
 
 export default class Presentation extends React.Component {
@@ -54,116 +53,101 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+          <Slide bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1}>
+              Exercices in
             </Heading>
             <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+              Programming Style
+            </Heading>
+          </Slide>
+          <Slide bgColor="black">
+            <Heading size={1} fit caps lineHeight={1}>
+              Why?
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={1} fit caps lineHeight={1}>
+              We are tired
             </Heading>
             <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
+              of programming flamewars
             </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+            <Image src={images.trollface.replace("/", "")} height="293px" padding="20px" bgColor="white" />
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
+          <Slide bgColor="black">
+            <Heading size={2} caps fit lineHeight={1} textColor="white">
+              What?
             </Heading>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
+          <Slide>
+            <Image src={images.book_programming.replace("/", "")} />
           </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
+          <Slide bgColor="black">
+            <Heading size={2} caps fit textColor="white">
+              Inspired from
+            </Heading>
+            <Image src={images.book_original.replace("/", "")} height="250pt"/>
+          </Slide>
+          <Slide>
+            <Heading size={1} caps>
+              By Raymond Queneau
+            </Heading>
+          </Slide>
+          <Slide bgColor="black">
+            <Image src={images.author_original.replace("/", "")} height="300pt" />
+            <Heading size={1}>
+              Handsome guy
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={1} caps fit>
+              One story
+            </Heading>
+            <Heading size={2} caps fit>
+              99 styles
+            </Heading>
+          </Slide>
+          <Slide bgColor="black">
+            <Heading size={1} caps fit>
+              What a style
+            </Heading>
+            <Heading size={1} caps fit>
+              really is?
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={1} caps fit>
+              Constraints!
+            </Heading>
+          </Slide>
+          <Slide bgImage={images.historical.replace("/", "")} bgDarken={0.75} notes="<ul><li>Good old times: small amount of primary memory; no identifiers</li><li>Go Forth: data stack; heap; procedures</li></ul>">
+            <Heading>Historical</Heading>
             <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
+              <Heading textColor="primary">Good old times</Heading>
             </Appear>
             <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
+              <Heading textColor="primary">Go Forth</Heading>
+            </Appear>
+          </Slide>
+          <Slide bgImage={images.basic.replace("/", "")} bgDarken={0.75}>
+            <Heading>Basic Styles</Heading>
+            <Appear fid="1">
+              <Heading textColor="primary">Monolithic</Heading>
+            </Appear>
+          </Slide>
+          <Slide bgImage={images.basic.replace("/", "")} bgDarken={0.75} notes="<ul><li>">
+            <Heading>Basic Styles</Heading>
+            <Heading textColor="primary">Monolithic</Heading>
+            <Appear fid="2">
+              <Heading textColor="primary">Cookbook</Heading>
             </Appear>
             <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
+              <Heading textColor="primary">Candy Factory</Heading>
             </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <Appear fid="4">
+              <Heading textColor="primary">Code Golf</Heading>
+            </Appear>
           </Slide>
         </Deck>
       </Spectacle>
