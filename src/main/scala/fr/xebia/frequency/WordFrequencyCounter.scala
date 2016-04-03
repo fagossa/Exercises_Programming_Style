1b0ded73a25cd0814cf4a6507e2eeab9cc2a47e4
Fabian Gutierrez <fagossa@gmail.com>
2016-04-03 16:43:27 +0200

    adds initial code based on granch 'pipeline'

--- src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
+++ src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
@@ -2,48 +2,43 @@ package fr.xebia.frequency
 
 object WordFrequencyCounter {
 
-  class WordFrequencyController(words: List[String], maxWords: Int) {
-
-    lazy val stopWords = scala.io.Source
-      .fromInputStream(getClass.getResourceAsStream("/stop_words.txt")).getLines()
-      .mkString.split(",").toList
-
-    def run: Seq[(String, Int)] = {
-      words
-        .map(_.replaceAll("[,|;|.]", ""))
-        // remove stop words
-        .flatMap(_.split(" "))
-        .map(_.toLowerCase)
-        .filter(!stopWords.contains(_))
-        .filter(_.forall(_.isLetter))
-        .filterNot(_.isEmpty)
-        //groupWordsTogether
-        .groupBy(identity)
-        .mapValues { t: List[String] => t.size }
-        //sortWords
-        .map { case (k, v) => (k, v) }.toSeq
-        .sortWith { case ((word1, count1), (word2, count2)) => if (count1 == count2) word1.compareTo(word2) <= 0 else count1 > count2 }
-        .take(maxWords)
-    }
+  def frequenciesOf(phrases: List[String], maxWords: Int): Seq[(String, Int)] = {
+    val triggerWith = removeSpecialChars _ andThen splitPhrases andThen removeStopWords andThen groupWordsTogether andThen sortWords(maxWords)
+    triggerWith(phrases)
   }
 
-  class WordFrequencyManager {
-    def groupWordsTogether(words: List[String]): Map[String, Int] =
-      words
-        .groupBy(identity)
-        .mapValues { t: List[String] => t.size }
+  def removeSpecialChars(phrases: List[String]): List[String] =
+    phrases
+      .map(_.replaceAll("[,|;|.|!]", ""))
 
-    def sortWords(maxWords: Int, frequency: Map[String, Int]): Seq[(String, Int)] = {
-      frequency.map { case (k, v) => (k, v) }.toSeq
-        .sortWith { case ((word1, count1), (word2, count2)) => if (count1 == count2) word1.compareTo(word2) <= 0 else count1 > count2 }
-        .take(maxWords)
-    }
+  def splitPhrases(phrases: List[String]): List[String] =
+    phrases
+      .flatMap(_.split(" "))
+      .map(_.toLowerCase)
+      .filter(_.forall(_.isLetter))
+      .filterNot(_.isEmpty)
+
+  def removeStopWords(phrases: List[String]): List[String] = {
+    val exclusions = scala.io.Source
+      .fromInputStream(getClass.getResourceAsStream("/stop_words.txt")).getLines()
+      .mkString.split(",").toList
+    phrases
+      .filterNot(exclusions.contains(_))
+  }
+
+  def groupWordsTogether(phrases: List[String]): Map[String, Int] =
+    phrases
+      .groupBy(identity)
+      .mapValues { t: List[String] => t.size }
+
+  def sortWords(maxWords: Int)(frequency: Map[String, Int]): Seq[(String, Int)] = {
+    frequency.map { case (k, v) => (k, v) }.toSeq
+      .sortWith { case ((word1, count1), (word2, count2)) => if (count1 == count2) word1.compareTo(word2) <= 0 else count1 > count2 }
+      .take(maxWords)
   }
 
   def main(args: Array[String]) {
-    val toList: List[String] = scala.io.Source.fromFile(args(0)).getLines().toList
-    new WordFrequencyController(toList, 25)
-      .run
+    WordFrequencyCounter.frequenciesOf(scala.io.Source.fromFile(args(0)).getLines().toList, 25)
       .foreach { case (word, frequency) => println(s"$word  -  $frequency") }
   }
 
--- src/test/scala/fr/xebia/frequency/WordFrequencyCounterSpec.scala
+++ src/test/scala/fr/xebia/frequency/WordFrequencyCounterSpec.scala
@@ -1,45 +1,43 @@
 package fr.xebia.frequency
 
-import fr.xebia.frequency.WordFrequencyCounter.WordFrequencyController
 import org.scalatest.{FunSpec, Matchers}
 
 class WordFrequencyCounterSpec extends FunSpec with Matchers {
 
   val count = 25
 
-  describe("Word frequency counter using hardcoded data") {
+  describe("Frequency counter tested integrally") {
     it("should ignore special characters") {
       val words = List("hEllo worlD worLd , to ; 4 (")
-
-      new WordFrequencyController(words, count).run should be(List(("world", 2), ("hello", 1)))
+      WordFrequencyCounter.frequenciesOf(words, count) should be(List(("world", 2), ("hello", 1)))
     }
 
     it("should handle several lines") {
-      val words = List(
+      val phrases = List(
         "White tigers live mostly in India",
         "Wild lions live mostly in Africa"
       )
-      new WordFrequencyController(words, count).run should be(List(
+      WordFrequencyCounter.frequenciesOf(phrases, count) should be(List(
         ("live", 2), ("mostly", 2),
         ("africa", 1), ("india", 1), ("lions", 1), ("tigers", 1), ("white", 1), ("wild", 1)
       ))
     }
 
-  }
-
-  describe("Word frequency counter reading data from a file") {
     lazy val words: List[String] = scala.io.Source
       .fromInputStream(getClass.getResourceAsStream("/mobydick.txt")).getLines().toList
 
     it("should find the most frequent words") {
-      new WordFrequencyController(words, count).run should be(List(
+      WordFrequencyCounter.frequenciesOf(words, count) should be(List(
         ("water", 6),
         ("whenever", 4),
         ("high", 3), ("land", 3), ("men", 3), ("nothing", 3), ("stand", 3), ("take", 3), ("upon", 3),
-        ("city", 2), ("come", 2), ("crowds", 2), ("find", 2), ("here", 2), ("little", 2), ("more", 2), ("myself", 2), ("ocean", 2), ("one", 2), ("reveries", 2), ("ships", 2), ("streets", 2), ("thousands", 2), ("time", 2), ("up", 2)
+        ("city", 2), ("come", 2), ("crowds", 2), ("find", 2), ("here", 2), ("little", 2), ("look", 2),
+        ("more", 2), ("myself", 2), ("ocean", 2), ("one", 2), ("reveries", 2), ("ships", 2), ("streets", 2),
+        ("thousands", 2), ("time", 2)
       ))
     }
 
   }
+
 
 }
