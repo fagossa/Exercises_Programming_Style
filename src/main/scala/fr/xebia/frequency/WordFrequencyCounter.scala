9ff9c7bb6051e0514fffa0941eb5afb73437a5b8
Fabian Gutierrez <fagossa@gmail.com>
2016-04-04 10:39:26 +0200

    removes solution

--- src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
+++ src/main/scala/fr/xebia/frequency/WordFrequencyCounter.scala
@@ -3,37 +3,7 @@ package fr.xebia.frequency
 object WordFrequencyCounter {
 
   def frequenciesOf(phrases: List[String], maxWords: Int): Seq[(String, Int)] = {
-    val triggerWith = removeSpecialChars _ andThen splitPhrases andThen removeStopWords andThen groupWordsTogether andThen sortWords(maxWords)
-    triggerWith(phrases)
-  }
-
-  def removeSpecialChars(phrases: List[String]): List[String] =
-    phrases.map(_.replaceAll("[,|;|.|!]", ""))
-
-  def splitPhrases(phrases: List[String]): List[String] =
-    phrases
-      .flatMap(_.split(" "))
-      .map(_.toLowerCase)
-      .filter(_.forall(_.isLetter))
-      .filterNot(_.isEmpty)
-
-  def removeStopWords(phrases: List[String]): List[String] = {
-    val exclusions = scala.io.Source
-      .fromInputStream(getClass.getResourceAsStream("/stop_words.txt")).getLines()
-      .mkString.split(",").toList
-    phrases
-      .filterNot(exclusions.contains(_))
-  }
-
-  def groupWordsTogether(phrases: List[String]): Map[String, Int] =
-    phrases
-      .groupBy(identity)
-      .mapValues { t: List[String] => t.size }
-
-  def sortWords(maxWords: Int)(frequency: Map[String, Int]): Seq[(String, Int)] = {
-    frequency.map { case (k, v) => (k, v) }.toSeq
-      .sortWith { case ((word1, count1), (word2, count2)) => if (count1 == count2) word1.compareTo(word2) <= 0 else count1 > count2 }
-      .take(maxWords)
+    ???
   }
 
   def main(args: Array[String]) {
